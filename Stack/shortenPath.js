a95d4ca8972b8f24612d7f543ec1ec426c1d40fd
quang-pth <57148627+quang-pth@users.noreply.github.com>
2021-05-29 18:14:54 +0700

    Difficulty: Hard

---
+++ Stack/shortenPath.js
@@ -0,0 +1,31 @@
+function shortenPath(path) {
+    const startWithSlash = path[0] === "/";
+    const tokens = path.split("/").filter(token => (token.length && token !== "."));
+    const trimmedPath = [];
+
+    if (startWithSlash) trimmedPath.push("");
+    
+    for (const token of tokens) {
+        if (token === "..") {
+            if (!trimmedPath.length || trimmedPath[trimmedPath.length - 1] === "..") {
+                trimmedPath.push(token);
+            }
+            else if (trimmedPath[trimmedPath.length - 1] !== "") {
+                trimmedPath.pop();
+            }
+        } else {
+            trimmedPath.push(token);
+        }
+    }
+
+    if (trimmedPath.length === 1 && trimmedPath[0] === "") return "/";
+
+    return trimmedPath.join("/");
+}
+
+console.log(shortenPath("/foo/../test/../test/../foo//bar/./baz"));
+// Output: /foo/bar/baz
+console.log(shortenPath("/../../foo/../../bar/baz"));
+// Output: /bar/baz
+console.log(shortenPath("../../foo/../../bar/baz"));
+// Output: ../../../bar/baz
