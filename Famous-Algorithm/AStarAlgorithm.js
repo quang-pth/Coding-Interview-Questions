f290fe2afcc4c4c4c1872ab67e09b16bbe518ba0
quang-pth <57148627+quang-pth@users.noreply.github.com>
2021-06-03 21:28:51 +0700

    Very Hard

--- Famous-Algorithm/AStarAlgorithm.js
+++ Famous-Algorithm/AStarAlgorithm.js
@@ -217,3 +217,10 @@ console.log(aStarAlgorithm(0, 1, 4, 3, [
     [1, 0, 1, 1, 1],
     [0, 0, 0, 0, 0]
   ]))
+// Shortest path from the begin to the end node: [
+//   [ 0, 1 ], [ 0, 0 ],
+//   [ 1, 0 ], [ 2, 0 ],
+//   [ 2, 1 ], [ 3, 1 ],
+//   [ 4, 1 ], [ 4, 2 ],
+//   [ 4, 3 ]
+// ]
